169af0cf5fd30cea977736d214e46cfe5c8e4571
fuuuuuuu28 <swordskill8@gmail.com>
2025-11-25 22:08:34 +0700

    13

--- frontend/vite-project/src/lib/axios.ts
+++ frontend/vite-project/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "production"
-    ? `${import.meta.env.VITE_SERVER_URI}/api`
+    ? `${import.meta.env.SERVER_URI}/api`
     : "http://localhost:5000/api",
     withCredentials:true,
 })
